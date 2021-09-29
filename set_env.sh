289f72fde83c7d08bdfda3762a72e3cf4c0fcc80
kelvinogirisen <kelvinogirisen@yahoo.com>
2021-09-29 15:39:32 +0100

    Initial commit

---
+++ set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=udagramkeldev
+export POSTGRES_PASSWORD=\#All4one#
+export POSTGRES_HOST=udagramkeldev.cvwbtnrcxxrq.us-east-1.rds.amazonaws.com
+export POSTGRES_DATABASE=postgres
+export AWS_MEDIA_BUCKET=udagram-kelvin-dev
+export AWS_REGION=us-west-2
+export AWS_PROFILE=default
+export JWT_SECRET=testing
+export URL=http://localhost:8100
